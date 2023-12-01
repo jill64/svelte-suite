605e3c35ba32171cad011db44db58244e5f490e5
jill64 <intents.turrets0h@icloud.com>
2023-12-01 23:21:28 +0900

    fix: exporting names

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-export * from '@jill64/svelte-dark-theme'
-export * from '@jill64/svelte-i18n'
-export * from '@jill64/svelte-menu'
-export * from '@jill64/svelte-ogp'
-export * from '@jill64/svelte-storage'
-export * from '@jill64/svelte-toast'
-export * from 'svelte-inline-modal'
-export * from 'svelte-qparam'
+export {
+  FlipButton,
+  ThemeManager,
+  flip,
+  onRender,
+  theme,
+  setting as themeSetting
+} from '@jill64/svelte-dark-theme'
+
+export {
+  LanguageManager,
+  LocaleAlternates,
+  init as i18nInit
+} from '@jill64/svelte-i18n'
+
+export { OGP, attach as ogpAttach } from '@jill64/svelte-ogp'
+export { define as defineQparam, qparam } from 'svelte-qparam'
+
+export { Menu } from '@jill64/svelte-menu'
+export { storage } from '@jill64/svelte-storage'
+export { Toaster, toast } from '@jill64/svelte-toast'
+export { InlineModal } from 'svelte-inline-modal'
